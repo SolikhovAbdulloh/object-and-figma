a578138566e160728313ff693b9636ee2eac7ed4
SolihovAbdulloh <abdullasolikh@mail.com>
2024-09-18 17:04:34 +0500

    finish

---
+++ js/main.js
@@ -0,0 +1,81 @@
+// let ism = "Xayrulloh";
+
+// let person = {
+//   ism,
+//   name: "Abdulloh",
+//   surname: "Solikhov",
+//   age: 25,
+//   height: 167,
+//   weight: 65,
+//   univercity:{
+//     course : 5,
+//     gpa : 4.8,
+
+//   }
+// };
+
+// // console.log(person);
+
+// delete person.surname
+
+// console.log(person.univercity ["course"]);
+
+// let count = 0
+// function calcSum(Obj) {
+//   for (a in Obj) {
+      
+      
+    
+//       return Object.values(Obj).reduce((total, value) => total + value, 0)
+  
+// }
+// }
+
+
+// let Obj = {
+//     John: 100,
+    
+//     Ann: 160,
+    
+//     Pete:130,
+// }
+
+
+// console.log(calcSum(Obj));
+
+
+// for(i in Obj){
+
+//     count += Obj[i]
+
+//     // console.log(Obj[key])
+// }
+
+// console.log(count);
+
+
+
+// const dasturchi = {
+//     ism:'ulug',
+//     yosh:25,
+//     sal: function(){
+//         console.log('salomat'+this.ism);
+        
+//     }
+// }
+// // console.log(dasturchi['ism']);
+// // console.log(dasturchi.sal());
+// console.log(Object.values(dasturchi));
+// console.log(Object.entries(dasturchi));
+
+ age = 34;
+let user = {
+    age:3,
+    name : 'anaba'
+    
+}
+console.log(user.work)
+
+    console.log(this);
+    
+
